7e070b7650dcf0545581775054562e659600d736
Alex Geis <siegxela@gmail.com>
2022-06-03 11:03:02 -0600

    add progress bar relevant HTMl and JS - still need to refactor to work properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,24 @@ hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("active");
 	navMenu.classList.toggle("active");
 });
+
+// working progress bar JS
+// let i = 0;
+// function move() {
+// 	if (i == 0) {
+// 		i = 1;
+// 		let elem = document.getElementById("myBar");
+// 		let width = 10;
+// 		let id = setInterval(frame, 10);
+// 		function frame() {
+// 			if (width >= 100) {
+// 				clearInterval(id);
+// 				i = 0;
+// 			} else {
+// 				width++;
+// 				elem.style.width = width + "%";
+// 				elem.innerHTML = width + "%";
+// 			}
+// 		}
+// 	}
+// }
