b9cb3df433f971626b6917cdb0e89b6dfc3a0bb8
Alex Geis <siegxela@gmail.com>
2022-10-18 09:46:06 -0600

    restructure server files into /server - adjust file paths to match

--- server/server.js
+++ server/server.js
@@ -15,10 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/api", api);
 
-app.use(express.static(path.join(__dirname, "client", "public")));
+app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
 // GET Route for homepage
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (req, res) =>
+	res.sendFile(path.join(__dirname, "..", "index.html"))
+);
 
 app.listen(PORT, () =>
 	console.log(`App listening at http://localhost:${PORT} 🚀`)
