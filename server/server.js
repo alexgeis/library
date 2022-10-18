ee06eda0e8b6bcccc5764e736e77d13fc6462761
Alex Geis <siegxela@gmail.com>
2022-10-18 10:37:53 -0600

    add jest dependency and /__tests__ folder with User.js model tests

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
 // const { clog } = require("./middleware/clog");
-// const api = require("./routes/index.js");
-
-const PORT = process.env.PORT || 3001;
-
-const app = express();
-
 // Import custom middleware, "cLog"
 // app.use(clog);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/api", api);
+// turn on routes
+app.use(routes);
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
@@ -22,6 +22,7 @@ app.get("/", (req, res) =>
 	res.sendFile(path.join(__dirname, "..", "index.html"))
 );
 
-app.listen(PORT, () =>
-	console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log("Now listening"));
+});
