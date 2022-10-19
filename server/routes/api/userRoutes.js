1bceb5d0d8d0deb39c17e80e3203dfe922d0b8eb
Alex Geis <siegxela@gmail.com>
2022-10-19 11:15:47 -0600

    add delete route to userRoutes.js

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -88,4 +88,22 @@ router.put("/:id", async (req, res) => {
 	}
 });
 
+// DELETE a user based on ID
+router.delete("/:id", async (req, res) => {
+	try {
+		const userData = await User.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+		if (!userData) {
+			res.status(404).json({ message: "No user with this id!" });
+			return;
+		}
+		res.status(200).json(userData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
