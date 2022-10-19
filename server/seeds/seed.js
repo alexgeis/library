62205c6f0cf1b10523de40ae039daba406f48085
Alex Geis <siegxela@gmail.com>
2022-10-19 09:30:19 -0600

    add seed data for Users and add logic to bulk create in seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,14 +1,14 @@
 const sequelize = require("../config/connection");
 const { User, Book } = require("../models");
 
-const seedData = require("./seedData.json");
+const userSeedData = require("./userSeedData.json");
 
 const seedDatabase = async () => {
 	await sequelize.sync({ force: true });
 
-	const drivers = await Driver.bulkCreate(seedData);
+	const users = await User.bulkCreate(userSeedData);
 
-	for (const { id } of drivers) {
+	for (const { id } of users) {
 		// Need to include a valid driver_id when creating a license
 		const newLicense = await License.create({
 			driver_id: id,
