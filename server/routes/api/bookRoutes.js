5adb21dad5afb680ce122fd6d173bb28107c0d26
Alex Geis <siegxela@gmail.com>
2022-10-19 13:15:43 -0600

    remove seeds from bookRoutes.js

--- server/routes/api/bookRoutes.js
+++ server/routes/api/bookRoutes.js
@@ -50,7 +50,6 @@ router.get("/:isbn", (req, res) => {
 });
 
 // CREATE a book
-//http://localhost:3001/api/books
 router.post("/", (req, res) => {
 	// Use Sequelize's `create()` method to add a row to the table
 	// Similar to `INSERT INTO` in plain SQL
@@ -65,62 +64,6 @@ router.post("/", (req, res) => {
 		.then((newBook) => {
 			// Send the newly created row as a JSON object
 			res.json(newBook);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
-});
-
-// CREATE multiple books
-//http://localhost:3001/api/books/seed
-router.post("/seed", (req, res) => {
-	// Multiple rows can be created with `bulkCreate()` and an array
-	// This could also be moved to a separate Node.js script to ensure it only happens once
-	Book.bulkCreate([
-		{
-			title: "Make It Stick: The Science of Successful Learning",
-			author: "Peter Brown",
-			isbn: "978-0674729018",
-			pages: 336,
-			edition: 1,
-			is_paperback: false,
-		},
-		{
-			title:
-				"Essential Scrum: A Practical Guide to the Most Popular Agile Process",
-			author: "Kenneth Rubin",
-			isbn: "978-0137043293",
-			pages: 500,
-			edition: 1,
-			is_paperback: true,
-		},
-		{
-			title: "The Pragmatic Programmer: Your Journey To Mastery",
-			author: "David Thomas",
-			isbn: "978-0135957059",
-			pages: 352,
-			edition: 2,
-			is_paperback: false,
-		},
-		{
-			title: "The Art of Computer Programming, Vol. 1: Fundamental Algorithms",
-			author: "Donald Knuth",
-			isbn: "978-0201896831",
-			pages: 672,
-			edition: 3,
-			is_paperback: false,
-		},
-		{
-			title: "Algorithms of Oppression: How Search Engines Reinforce Racism",
-			author: "Safiya Umoja Noble",
-			isbn: "978-1479837243",
-			pages: 256,
-			edition: 1,
-			is_paperback: true,
-		},
-	])
-		.then(() => {
-			res.send("Database seeded!");
 		})
 		.catch((err) => {
 			res.json(err);
