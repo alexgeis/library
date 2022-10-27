49b96618a156eba7de5d8453b6fd1a45507ce57f
Alex Geis <siegxela@gmail.com>
2022-10-27 07:41:49 -0600

    refactor book class logic into bookClass.js

---
+++ client/public/js/bookClass.js
@@ -0,0 +1,23 @@
+//BOOK CONSTRUCTOR
+function Book(title, author, pages, language, publishDate, readStatus) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.language = language;
+	this.publishDate = publishDate;
+	this.readStatus = readStatus;
+	this.insertion_date = new Date().toLocaleString();
+}
+
+// CLASS VERSION OF ABOVE CODE 84-93//
+// class Book {
+// 	constructor(title, author, pages, language, publishDate, readStatus) {
+// 		this.title = title;
+// 		this.author = author;
+// 		this.pages = pages;
+// 		this.language = language;
+// 		this.publishDate = publishDate;
+// 		this.readStatus = readStatus;
+// 		this.insertion_date = new Date().toLocaleString();
+// 	}
+// }
