5b331c5e6547ede21277f88a2f9b3849ab4ad18f
Alex Geis <siegxela@gmail.com>
2022-10-30 20:23:28 -0600

    capturing user input on login form logic and username/email validation

--- client/public/js/loginSignupForm.js
+++ client/public/js/loginSignupForm.js
@@ -8,46 +8,10 @@ function renderLoginSection() {
 const loginBtn = document.querySelector("#login");
 loginBtn.addEventListener("click", renderLoginSection);
 
-// NEW LOGIN FORM
 const newLoginForm = document.querySelector(".login-form");
 const loginEmailForm = document.querySelector("#login-email");
+const loginUsernameForm = document.querySelector("#login-username");
 const loginPasswordForm = document.querySelector("#login-password");
-const loginFormBtn = document.querySelector("#login-btn");
-
-//ADD USER FUNCTION
-const createUser = async function(e) {
-	e.preventDefault();
-	
-	const newUser = new User(loginEmailForm.value, loginPasswordForm.value);
-if ((bookReadStatusForm.value === true))
-const response = postData("/api/users");
-console.log(response);
-
-currentBooks.push(newUser);
-setCurrentBooks(currentUsers);
-console.log(currentUsers);
-localStorage.setItem("books", JSON.stringify(currentBooks));
-addBookSection.setAttribute("style", "display: none;");
-setBookReadTotals();
-renderBooks();
-}
-
-const newPost = async function (event) {
-	event.preventDefault();
-	const title = document.querySelector('input[name="post-title"]').value;
-	const body = document.querySelector('textarea[name="post-body"]').value;
-  
-	await fetch(`/api/post`, {
-	  method: "POST",
-	  body: JSON.stringify({
-		title,
-		body,
-	  }),
-	  headers: { "Content-Type": "application/json" },
-	});
-  
-	document.location.replace("/dashboard");
-  };
 
 const closeLoginFormBtn = document.querySelector("#close-login-form");
 const clearLoginFormBtn = document.querySelector("#clear-login-form");
@@ -56,19 +20,61 @@ function clearLoginForm() {
 	loginEmailForm.value = "";
 	loginPasswordForm.value = "";
 }
+clearLoginFormBtn.addEventListener("click", clearLoginForm);
 function closeLoginForm() {
 	loginSection.setAttribute("style", "display: none;");
 }
+closeLoginFormBtn.addEventListener("click", closeLoginForm);
 
 const loginToSignupBtn = document.querySelector("#login-to-signup-form");
 loginToSignupBtn.addEventListener("click", renderSignupSection);
 
+//ADD USER FUNCTION
+const createUser = async function (e) {
+	e.preventDefault();
+	const email = loginEmailForm.value;
+	const password = loginPasswordForm.value;
+	const username = loginUsernameForm.value;
+
+	const newUser = new User(email, password);
+
+	const loginErrMsg = document.querySelector("#login-err-no-user-email");
+	if (email === "" && username === "") {
+		loginErrMsg.setAttribute("style", "display: block;");
+	}
+
+	// const response = postData("/api/users");
+
+	// old logic
+	currentBooks.push(newUser);
+	setCurrentBooks(currentUsers);
+	console.log(currentUsers);
+	localStorage.setItem("books", JSON.stringify(currentBooks));
+	addBookSection.setAttribute("style", "display: none;");
+	setBookReadTotals();
+	renderBooks();
+};
+
+const newPost = async function (event) {
+	event.preventDefault();
+	const title = document.querySelector('input[name="post-title"]').value;
+	const body = document.querySelector('textarea[name="post-body"]').value;
+
+	await fetch(`/api/post`, {
+		method: "POST",
+		body: JSON.stringify({
+			title,
+			body,
+		}),
+		headers: { "Content-Type": "application/json" },
+	});
+
+	document.location.replace("/dashboard");
+};
+const loginFormBtn = document.querySelector("#login-btn");
 loginFormBtn.addEventListener("click", addUser);
-clearLoginFormBtn.addEventListener("click", clearLoginForm);
-closeLoginFormBtn.addEventListener("click", closeLoginForm);
 
 // SIGNUP FORM
-//
 function renderSignupSection() {
 	loginSection.setAttribute("style", "display: none;");
 	signupSection.setAttribute("style", "display: block;");
