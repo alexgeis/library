7f7ac4cc66d8442cf6b26833b3da276ff7126451
Alex Geis <siegxela@gmail.com>
2022-10-31 17:52:09 -0600

    finish search bar logic

--- client/public/js/requestMethods.js
+++ client/public/js/requestMethods.js
@@ -1,3 +1,12 @@
+//GET
+async function getData(url = "") {
+	const response = await fetch(url, {
+		mode: "cors",
+		credentials: "same-origin",
+	});
+	return response.json(); // parses JSON response into native JavaScript objects
+}
+
 //POST
 async function postData(url = "", data = {}) {
 	const response = await fetch(url, {
--- client/public/js/search.js
+++ client/public/js/search.js
@@ -1,76 +1,21 @@
 // SEARCH BAR
 function renderSearch(search) {
 	if (search === "") return;
-	//get all tasks from all projects
-	const currentProjects = JSON.parse(localStorage.getItem("projects")) || [];
-	const currentTasks = [];
-	for (let i = 0; i < currentProjects.length; i++) {
-		const project = currentProjects[i];
-		currentTasks.push(...project.tasks);
-	}
-	const filterTasks = [];
-	for (let i = 0; i < currentTasks.length; i++) {
-		const task = currentTasks[i];
-		let lowerTitle = task.title.toLowerCase();
+
+	const allBooks = getData("/api/books");
+
+	const filterBooks = [];
+	for (let i = 0; i < allBooks.length; i++) {
+		const book = allBooks[i];
+		let lowerTitle = book.title.toLowerCase();
 		let lowerSearch = search.toLowerCase();
 		if (lowerTitle.includes(lowerSearch)) {
-			filterTasks.push(task);
+			filterBooks.push(book);
 		}
 	}
-	//update main header
-	const mainHeader = document.querySelector("#main-header");
-	mainHeader.textContent = "SEARCH RESULTS";
-	//clear current tasks
-	const taskContainer = document.querySelector("#main-content");
-	taskContainer.innerHTML = "";
-	//no search results backup
-	if (filterTasks.length === 0) {
-		taskContainer.textContent = "No search results";
-	}
-	//render filterTasks tasks
-	for (let i = 0; i < filterTasks.length; i++) {
-		const task = filterTasks[i];
-		const taskCard = domCreate("div", ["task-card"], { "data-task-index": i });
-		//inner task card div items
-		//TITLE
-		const taskTitle = domCreate("div", ["task-title"]);
-		taskTitle.textContent = task.title;
-		//DESCRIPTION
-		const taskDesc = domCreate("div", ["task-description"]);
-		taskDesc.textContent = task.description;
-		//DUE DATE
-		const taskDate = domCreate("div", ["task-date"]);
-		taskDate.textContent = task.dueDate;
-		//PRIORITY
-		const taskPriority = domCreate("div", ["task-priority"]);
-		taskPriority.textContent = task.priority;
-		//NOTES
-		const taskNotes = domCreate("div", ["task-notes"]);
-		taskNotes.textContent = task.notes;
-		//DELETE ICON
-		const taskDeleteIcon = domCreate("img", ["task-delete"], {
-			src: deleteIcon,
-			alt: "trash can delete icon",
-		});
-		taskDeleteIcon.addEventListener("click", function () {
-			taskDeleteIcon.parentElement.remove(taskCard);
-			let index = taskDeleteIcon.parentElement.getAttribute("data-task-index");
-			filterTasks.splice(index, 1);
-			// localStorage.setItem("filterTasks", JSON.stringify(filterTasks));
-			renderSearch(search);
-		});
-		//APPEND ELEMENTS
-		taskCard.append(
-			taskTitle,
-			taskDesc,
-			taskDate,
-			taskPriority,
-			taskNotes,
-			taskDeleteIcon
-		);
-		taskContainer.appendChild(taskCard);
-	}
-	// return taskContainer;
+
+	setCurrentBooks(filterBooks);
+	renderBooks();
 }
 
 const searchInput = document.querySelector("#book-search");
