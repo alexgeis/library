7108205865a323ddf7c851e14210aa61cfcbeeed
Alex Geis <siegxela@gmail.com>
2022-11-01 14:52:18 -0600

    add express-session dependency and add boilerplate session set up logic in server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
@@ -9,6 +10,14 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up sessions
+const sess = {
+	secret: "Super secret secret",
+	resave: false,
+	saveUninitialized: true,
+};
+
+app.use(session(sess));
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
