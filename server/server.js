7e73d24b99261899b5c15484f245cc3f55820b10
Alex Geis <siegxela@gmail.com>
2022-11-02 09:19:12 -0600

    add cookie info to server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const session = require("express-session");
 const path = require("path");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 // const { clog } = require("./middleware/clog");
 // Import custom middleware, "cLog"
 // app.use(clog);
@@ -10,14 +12,22 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up sessions
+// Set up sessions with cookies
 const sess = {
 	secret: "Super secret secret",
+	cookie: {
+		// Stored in milliseconds (86400 === 1 day)
+		maxAge: 86400,
+	},
 	resave: false,
 	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
 };
 
 app.use(session(sess));
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
