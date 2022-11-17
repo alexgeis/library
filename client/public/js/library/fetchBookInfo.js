a5461caab25e6d6a406347fda2a9c6f48accd6db
Alex Geis <siegxela@gmail.com>
2022-11-17 09:04:38 -0700

    add fetchBookInfo.js and open library api example fetch

---
+++ client/public/js/library/fetchBookInfo.js
@@ -0,0 +1,12 @@
+const fetchBookInfo = async (query) => {
+	const libUrlAPI = "http://openlibrary.org/search.json?q=";
+	const cleanQueryStr = query.split(" ").join("+");
+	const libAPIQueryStr = libUrlAPI + cleanQueryStr;
+
+	const responseLibraryData = await fetch(libAPIQueryStr);
+	const responseLibrary = await responseLibraryData.json();
+
+	console.log(responseLibrary);
+};
+
+fetchBookInfo("lord of the rings");
