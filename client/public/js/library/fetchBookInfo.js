b73dc4f4f0faad82b10c4230e7764040fcbdb8a5
Alex Geis <siegxela@gmail.com>
2022-11-17 09:38:27 -0700

    adjust loading order of scripts so the fetch book info function comes before the search logic (so it can be used with user search)

--- client/public/js/library/fetchBookInfo.js
+++ client/public/js/library/fetchBookInfo.js
@@ -6,7 +6,8 @@ const fetchBookInfo = async (query) => {
 	const responseLibraryData = await fetch(libAPIQueryStr);
 	const responseLibrary = await responseLibraryData.json();
 
-	console.log(responseLibrary);
+	// console.log(responseLibrary);
+	return responseLibrary;
 };
 
 fetchBookInfo("lord of the rings");
