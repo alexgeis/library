c95457a253243f315d8a0eef5f5a87b469d6a081
Alex Geis <siegxela@gmail.com>
2022-11-25 15:58:36 -0700

    add book test - check for book title and book author

--- server/__tests__/Book.test.js
+++ server/__tests__/Book.test.js
@@ -21,26 +21,32 @@ test("Checks for null values", async () => {
 	await expect(newBook2.validate()).resolves.not.toThrow();
 });
 
-test("Checks for short passwords", async () => {
-	const user1 = {
-		username: "test",
-		email: "test@test.com",
-		password: "123",
+test("Checks for book title and book author (required fields)", async () => {
+	const book1 = {
+		isbn: "9780099533934",
+		pages: "593",
+		edition: "3",
+		publish_date: "1979",
+		is_read: "false",
 	};
 
-	const user2 = {
-		username: "test",
-		email: "test@test.com",
-		password: "password123",
+	const book2 = {
+		title: "Gravity's Rainbow",
+		author: "Thomas Pynchon",
+		isbn: "9780099533214",
+		pages: "760",
+		edition: "1",
+		publish_date: "1973",
+		is_read: "true",
 	};
 
-	const newUser1 = User.build(user1);
-	const newUser2 = User.build(user2);
+	const newBook1 = Book.build(book1);
+	const newBook2 = Book.build(book2);
 
-	await expect(newUser1.validate()).rejects.toThrow(
-		"Validation len on password failed"
+	await expect(newBook1.validate()).rejects.toThrow(
+		"Validation - book title and author required"
 	);
-	await expect(newUser2.validate()).resolves.not.toThrow();
+	await expect(newBook2.validate()).resolves.not.toThrow();
 });
 
 test("Checks for alphanumeric username", async () => {
