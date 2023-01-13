6806c7ba967e52f0f0b1298c6c86cd22c5d6bc00
Alex Geis <siegxela@gmail.com>
2023-01-12 17:22:29 -0700

    remove unused code from client/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,10 @@
 import styles from "./css/homepageStyle.css";
 
-import { loginUserHandler } from "./js/login/login"
+import { loginUserHandler } from "./js/login/login";
 const loginSubmitBtn = document.querySelector(".login-submit-btn");
 
 loginSubmitBtn.addEventListener("click", loginUserHandler);
 
-import bookshelfIcon from "./assets/icons/book-open-page-variant-outline.svg"
+import bookshelfIcon from "./assets/icons/book-open-page-variant-outline.svg";
 
 document.querySelector("#lib-icon-home-page").src = bookshelfIcon;
-
-
-// window.onload = () => {
-// 	setCurrentTheme(DEFAULT_THEME);
-// 	setCurrentBooks(currentBooks);
-// 	renderBooks();
-// };
